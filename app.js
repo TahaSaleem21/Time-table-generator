883edbe8a5e8c1549726b57ea9d215cd1dc4a41b
Taha Saleem <tstahasaleem@gmail.com>
2024-04-19 16:44:06 -0700

    Last commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function generateTable() {
+  var number = parseInt(document.getElementById("number").value);
+  var tableContainer = document.getElementById("table-container");
+  tableContainer.innerHTML = "";
+
+  var table = document.createElement("table");
+  for (var i = 1; i <= 10; i++) {
+    var row = document.createElement("tr");
+    row.innerHTML =
+      "<td>" + number + " x " + i + "</td>" + "<td>" + number * i + "</td>";
+    table.appendChild(row);
+  }
+  tableContainer.appendChild(table);
+}
